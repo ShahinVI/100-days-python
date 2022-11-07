eec3bddceab784af0945975142993be3b6d2a5bc
ShahinVI <shahinaiman@gmail.com>
2022-11-07 01:04:07 +0100

    life in weeks

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # 100-days-python
 this repository will contain all the challenges of the 100 days of python
 
-    1-  Challenge 1 Band name generator [x]
+    1-  Challenge 1 Band name generator         [x]
+    2-  Challenge 2 age in weeks                [x]
---
+++ challenge 2 life in weeks/main.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+left = 90-int(age)
+month = left*12
+week = int(left *52)
+day = left*365
+
+print("You have {} days, {} weeks, or {} months left.".format(day,week,month))
+# this is a humbling prespective that is why i uploaded it
