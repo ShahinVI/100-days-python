3f43e0a24316a783acac5c41c843b1863cd84c23
ShahinVI <shahinaiman@gmail.com>
2024-10-05 11:23:01 +0200

    corrected challenge 9_3

--- challenge 9_3 love calculator/main.py
+++ challenge 9_3 love calculator/main.py
@@ -1,10 +1,14 @@
 def calculate_love_score(name1,name2):
+    count1=0
+    count2=0
+    count3=0
+    count4=0
     for i in "true":
-        count1 = name1.count(i)
-        count2 = name2.count(i)
+        count1 += name1.count(i)
+        count2 += name2.count(i)
     for i in "love":
-        count3 = name1.count(i)
-        count4 = name2.count(i)
+        count3 += name1.count(i)
+        count4 += name2.count(i)
     print(f"Your love score is {count1+count2}{count3+count4}")
 def main():
     game_on = True
